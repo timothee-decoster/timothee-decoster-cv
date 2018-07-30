5367216dc5fe25faee921ccc7f4f49752d369d81
decoster <timothee.decoster@elosi.com>
2018-07-30 16:06:01 +0200

    Add profile img

--- src/components/Nav-header.vue
+++ src/components/Nav-header.vue
@@ -1,21 +1,26 @@
 <template>
   <div id='nav-header'>
+    <img src="../assets/profile.jpg" id="profile">
     <h1>{{ msg }}</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Nav-header",
-  data() {
-    return {
-      msg: "Side naviguation header"
-    };
-  }
+    name: 'Nav-header',
+    data() {
+        return {
+            msg: 'Timothée Decoster'
+        };
+    }
 };
 </script>
 
 <style scoped>
+#profile {
+    width: 50%;
+    border-radius: 50%;
+}
 h1 {
     color: beige;
 }
