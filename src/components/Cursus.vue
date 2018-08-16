e1151254816f5af0370d364a2e264e15e3cd5090
decoster <timothee.decoster@elosi.com>
2018-08-16 10:25:24 +0200

    Cursus (ToDo)

--- src/components/Cursus.vue
+++ src/components/Cursus.vue
@@ -1,7 +1,17 @@
 <template>
-  <div id="cursus">
-    <h1><b>Cursus</b></h1>
-  </div>
+    <div id="cursus">
+        <h1><b>Cursus scolaire</b></h1>
+        <div class="cursus">
+            <ul>
+                <li>Juillet 2014 : </li>
+                <li>Juin 2018 : </li>
+            </ul>
+            <ul>
+                <li><b>BAC S</b> option Si (lycée St Jude & St Louis)</li>
+                <li>Licence 3 Informatique (université de Lille)</li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -15,7 +25,12 @@ h1 {
     font-weight: normal;
     font-size: 50px;
 }
-p {
+.cursus {
+    justify-content: center;
+    display: flex;
+}
+li {
+    margin: 25px;
     font-size: 25px;
 }
 </style>
