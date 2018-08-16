b6b1d13714a1c6807747ae37d95453bb9d1080b5
decoster <timothee.decoster@elosi.com>
2018-08-16 11:26:10 +0200

    Cursus

--- src/components/Cursus.vue
+++ src/components/Cursus.vue
@@ -1,16 +1,20 @@
 <template>
     <div id="cursus">
         <h1><b>Cursus scolaire</b></h1>
-        <div class="cursus">
-            <ul>
-                <li>Juillet 2014 : </li>
-                <li>Juin 2018 : </li>
-            </ul>
-            <ul>
-                <li><b>BAC S</b> option Si (lycée St Jude & St Louis)</li>
-                <li>Licence 3 Informatique (université de Lille)</li>
-            </ul>
-        </div>
+        <table class="cursus">
+            <tr>
+                <td><b>Juillet 2014</b> : </td>
+                <td>BAC S option Si (lycée St Jude & St Louis)</td>
+                <td><img src="@/assets/st_jude.jpg" alt="Saint Jude" width="75px" height="75px"></td>
+            </tr>
+            <tr>
+                <td><b>Juin 2018</b> : </td>
+                <td>Licence 3 Informatique (université de Lille)</td>
+                <td><img src="@/assets/univ_lille.png" alt="Université de Lille" width="150px"></td>
+            </tr>
+        </table>
+        <h2><b>Actuellement</b></h2>
+        <p>Master MEEF 2° degrée Maths option Informatique</p>
     </div>
 </template>
 
@@ -21,16 +25,27 @@ export default {
 </script>
 
 <style scoped>
-h1 {
+h1,
+h2 {
     font-weight: normal;
     font-size: 50px;
 }
 .cursus {
-    justify-content: center;
     display: flex;
+    flex-direction: column;
+    align-items: center;
 }
-li {
-    margin: 25px;
+tr {
+    display: flex;
+    align-items: center;
+    margin: 20px;
     font-size: 25px;
+}
+td {
+    margin-right: 50px;
+    margin-left: 50px;
+}
+p {
+    font-size: 25px
 }
 </style>
